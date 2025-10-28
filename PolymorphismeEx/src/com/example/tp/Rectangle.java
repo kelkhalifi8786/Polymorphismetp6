cd3ca7de8f40634f5adfcb400ef0b40703d43a22
pigeo <pigeo@omar>
2025-10-29 00:56:21 +0100

    initial commit

---
+++ PolymorphismeEx/src/com/example/tp/Rectangle.java
@@ -0,0 +1,20 @@
+package com.example.tp;
+
+/**
+ * Rectangle de largeur et de hauteur données.
+ */
+public class Rectangle extends Forme {
+    private double largeur, hauteur;
+
+    public Rectangle(String couleur, double largeur, double hauteur) {
+        super(couleur);
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+    }
+
+    @Override
+    public void dessiner() {
+        System.out.println("Dessiner un rectangle de couleur " + couleur +
+                ", largeur=" + largeur + ", hauteur=" + hauteur);
+    }
+}
